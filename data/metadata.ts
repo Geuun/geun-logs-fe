04a2c78bebbffe267ad5be3869bcf07d4bcb7d6e
Geuun <jgk9612@gmail.com>
2023-02-28 03:53:28 +0900

    refactor: Container Component 수정
    1. logo image 추가
    2. metadata 추가
    
    index에 반영

---
+++ data/metadata.ts
@@ -0,0 +1,5 @@
+const metadata = {
+  title: 'Geun Logs',
+};
+
+export default metadata;
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import Container from 'components/Container';
 const Home = () => {
   return (
     <Container>
-      <span className={`text-green-500 text-lg`}>Home</span>
+      <span className={`font-bold`}>Home</span>
     </Container>
   );
 };
